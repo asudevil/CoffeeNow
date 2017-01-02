d2da66d2390996e8d5c0cfd2b7786e8e76845eb1
admin <admin@admins-MacBook-Pro.local>
2017-01-01 21:47:58 -0700

    removed the check for duplicate contact to update contact.  Creates new contact instead

--- CoffeeNow/Contacts+Ext+Handlers.swift
+++ CoffeeNow/Contacts+Ext+Handlers.swift
@@ -28,96 +28,70 @@ extension UserProfileCollectionVC {
             print("Not handled")
         }
     }
+
+
     func saveContact(contactInfo: [String: Any]) {
-
-        var contact = CNMutableContact()
-        let request = CNSaveRequest()
-        let keysToFetch = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactEmailAddressesKey, CNContactPhoneNumbersKey, CNContactNoteKey, CNContactSocialProfilesKey, CNContactJobTitleKey]
         
-        var givenName = String()
-        var familyName = String()
-        var phone = "Not Available"
+        let contact = CNMutableContact()
+        
         if let firstName = contactInfo["firstName"] as? String {
-            givenName = firstName
+            contact.givenName = firstName
         }
+
         if let lastName = contactInfo["lastName"] as? String {
-            familyName = lastName
-        }
-        if let phoneNumber = contactInfo["phone"] as? String {
-            phone = phoneNumber
-        }
-        
-//        let predicate = CNContact.predicateForContacts(matchingName: "\(givenName) \(familyName)")
-        let predicate2 = CNContact.predicateForContacts(withIdentifiers: [givenName, familyName, phone])
-        
-        do {
-            let contacts = try store.unifiedContacts(matching: predicate2, keysToFetch: keysToFetch as [CNKeyDescriptor])
-            print("Contacts fetched.", contacts.count)
-            if let output = contacts.first {
-                contact = output.mutableCopy() as! CNMutableContact
-                createContact(contactInfoToCreate: contactInfo, cnContact: contact)
-                request.update(contact)
-                print("User already exists so updating contact info")
-            } else {
-                createContact(contactInfoToCreate: contactInfo, cnContact: contact)
-                request.add(contact, toContainerWithIdentifier: nil)
-                print("Adding new contact to list!")
-            }
-        }
-        catch {
-            print("Unable to fetch contacts.")
-        }
-        
-        do{
-            try store.execute(request)
-            print("Successfully added or updated the contact")
-        } catch let err{
-            print("Failed to save the contact. \(err)")
-        }
-    }
-
-    func createContact(contactInfoToCreate: [String: Any], cnContact: CNMutableContact) {
-        
-        if let firstName = contactInfoToCreate["firstName"] as? String {
-            cnContact.givenName = firstName
+            contact.familyName = lastName
         }
 
-        if let lastName = contactInfoToCreate["lastName"] as? String {
-            cnContact.familyName = lastName
-        }
-
-        if let imgUrl = contactInfoToCreate["imageUrl"] as? String {
+        if let imgUrl = contactInfo["imageUrl"] as? String {
             let imgView = UIImageView()
             imgView.loadImageUsingCacheWithUrlString(urlString: imgUrl)
             if let img = imgView.image,
                 let imgData = UIImagePNGRepresentation(img){
-                cnContact.imageData = imgData
+                contact.imageData = imgData
             }
         }
 
-        if let email = contactInfoToCreate["email"] as? String {
+        if let email = contactInfo["email"] as? String {
             let email = email as NSString
             let homeEmail = CNLabeledValue(label: CNLabelHome, value: email)
-            cnContact.emailAddresses = [homeEmail]
+            contact.emailAddresses = [homeEmail]
         }
 
-        if let phone = contactInfoToCreate["phone"] as? String {
-            cnContact.phoneNumbers = [CNLabeledValue(label:CNLabelPhoneNumberiPhone, value:CNPhoneNumber(stringValue:phone))]
+        if let phone = contactInfo["phone"] as? String {
+            contact.phoneNumbers = [CNLabeledValue(label:CNLabelPhoneNumberiPhone, value:CNPhoneNumber(stringValue:phone))]
         }
 
-        if let notes = contactInfoToCreate["details"] as? String {
-            cnContact.note = notes
+        if let notes = contactInfo["details"] as? String {
+            contact.note = notes
         }
 
         //Social Media Profiles:
-        if let linkedInUserName = contactInfoToCreate["linkedIn"] as? String {
+        if let linkedInUserName = contactInfo["linkedIn"] as? String {
             let linkedInProfile = CNLabeledValue(label: "LinkedIn", value: CNSocialProfile(urlString: nil, username: linkedInUserName, userIdentifier: nil, service: CNSocialProfileServiceLinkedIn))
         
-            cnContact.socialProfiles = [linkedInProfile]   // [facebookProfile, twitterProfile, linkedInProfile]
+            contact.socialProfiles = [linkedInProfile]   // [facebookProfile, twitterProfile, linkedInProfile]
         }
         
-        if let jobTitle = contactInfoToCreate["occupation"] as? String {
-            cnContact.jobTitle = jobTitle
+        if let jobTitle = contactInfo["occupation"] as? String {
+            contact.jobTitle = jobTitle
+        }
+        let request = CNSaveRequest()
+
+        request.add(contact, toContainerWithIdentifier: nil)
+        
+        do{
+            try store.execute(request)
+            let alertTitle = "Contact Added!"
+            let alertMsg = "Successfully added the contact to your iPhone contacts.  Please check your iphone contacts to verify"
+            print(alertTitle)
+            self.alertTaskComplete(alertTitle: alertTitle, alertMessage: alertMsg)
+            
+        } catch let err{
+            let alertTitle = "ERROR ADDING CONTACT"
+            let alertMsg = "Failed to save the contact.  Please check the information and try again"
+            print(alertTitle)
+            self.alertTaskComplete(alertTitle: alertTitle, alertMessage: alertMsg)
+            print(alertTitle, err)
         }
     }
 }
